92245a75e133713e3b5a4349a7f7553d27bff4af
markelov <markelovss@bfa-d.ru>
2023-11-13 17:41:49 +0300

    start project

---
+++ src/test/java/ru/bfad/bfaApps/inventoryApp/InventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bfad.bfaApps.inventoryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
